718ed396fd9d1e8ded8eecbd8b92df6f17c9f346
Hrishikesh Rana <itsranahrishikesh@gmail.com>
2025-03-02 13:11:21 +0530

    Added onboarding page

---
+++ app/(platform)/onboarding/page.tsx
@@ -0,0 +1,19 @@
+import { redirect } from "next/navigation";
+import { industries } from "@/src/data/industries";
+import OnboardingForm from "@/src/components/onboarding/onboarding-form";
+import { getUserOnboardingStatus } from "@/src/actions/user";
+
+export default async function OnboardingPage() {
+  // Check if user is already onboarded
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <main>
+      <OnboardingForm industries={industries} />
+    </main>
+  );
+}
